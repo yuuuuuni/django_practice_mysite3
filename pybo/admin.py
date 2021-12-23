5ff646b71a2bcf0236693f8698168046cda175bb
yuuuuuni <yuuuuuni930302@gmail.com>
2021-12-24 00:39:15 +0900

    2-4 질문 목록까지

--- pybo/admin.py
+++ pybo/admin.py
@@ -3,5 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question, Answer
 
-admin.site.register(Question)
-admin.site.register(Answer)
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['subject']
+
+class AnswerAdmin(admin.ModelAdmin):
+    search_fields = ['content']
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer, AnswerAdmin)
--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,12 @@
-from django.http import HttpResponse
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from pybo.models import Question
+
+
 def index(request):
-    return HttpResponse("하이하이")
+    """
+    pybo 목록 출력
+    """
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
