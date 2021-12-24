2959fd0fdae93dd27a5c6ea9f7e9a95ae5d3f0b7
yuuuuuni <yuuuuuni930302@gmail.com>
2021-12-24 13:50:51 +0900

    2-4 detail 함수 추가할 차례

--- config/settings.py
+++ config/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mysite3', # DB명
-        'USER': 'root', # DBMS 접속 아이디 (SQLyog의 사용자이름이랑 일치, 기본값 : root)
-        'PASSWORD': '', # # DBMS 접속 비번 (SQLyog의 비밀번호랑 일치, 기본값 : '')
+        'USER': 'sbsst', # DBMS 접속 아이디 (SQLyog의 사용자이름이랑 일치, 기본값 : root)
+        'PASSWORD': 'sbs123414', # # DBMS 접속 비번 (SQLyog의 비밀번호랑 일치, 기본값 : '')
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
--- pybo/views.py
+++ pybo/views.py
@@ -7,6 +7,9 @@ def index(request):
     """
     pybo 목록 출력
     """
-    question_list = Question.objects.order_by('-create_date')
+    question_list = Question.objects.order_by('-create_date') # 질문 목록 데이터를 question_list라는 변수에 넣어줌
     context = {'question_list': question_list}
     return render(request, 'pybo/question_list.html', context)
+
+
+def detail(request, question_id):
