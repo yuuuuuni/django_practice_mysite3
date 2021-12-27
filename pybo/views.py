66b6d474e76e0274e7979d7dfb2ef20d25d234aa
yuuuuuni <yuuuuuni930302@gmail.com>
2021-12-27 13:47:24 +0900

    2-10 답변등록 부분 오류남

--- pybo/views.py
+++ pybo/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 
-from .forms import QuestionForm
+from .forms import QuestionForm, AnswerForm
 from .models import Question
 
 
@@ -33,17 +33,25 @@ def answer_create(request, question_id):
     pybo 답변등록
     """
     question = get_object_or_404(Question, pk=question_id)
-    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
-    return redirect('pybo:detail', question_id=question.id)
+    if request.method == 'POST':
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        form = AnswerForm()
+    context = {'question': question, 'form': form}
+    return render(request, 'pybo/question_detail.html', context)
 
 
 def question_create(request):
     """
     pybo 질문등록
     """
-    # 여기부터 다시 써보기
     if request.method == 'POST':
-        form = QuestionForm(request.POST)
+        form = QuestionForm(request.POST) # request.POST는 화면에서 사용자가 입력한 내용들이 담겨있음
         if form.is_valid():
             question = form.save(commit=False)
             question.create_date = timezone.now()
